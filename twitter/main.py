a09fc0014151e266d2afc99915dff9da197d6b5b
Lokesh Sai Sri Harsha Sankarasetty <lsankarasett@student.unimebl.edu.au>
2020-05-25 16:33:47 +1000

    Enabling CORS from all domains

--- twitter/main.py
+++ twitter/main.py
@@ -2,7 +2,7 @@ import os
 
 import uvicorn
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
 
 from routes import *
 
