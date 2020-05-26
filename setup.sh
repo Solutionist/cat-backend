df68ab1ff64e0ad4d870d1f7aac316183ef9c5f1
Lokesh Sai Sri Harsha Sankarasetty <lsankarasett@student.unimebl.edu.au>
2020-05-27 00:49:02 +1000

    Added Names to all files

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# COMP90024 Project - Team 34
+# Lokesh Sai Sri Harsha Sankarasetty, Melbourne, [1130612]
+# Kanch Vatcharotayan, Melbourne, [1132855]
+# Sai Deepthi Amancha, Melbourne, [1051388]
+# Josin Saji Abraham, Melbourne, [1129428]
+# Kush Garg, Melbourne, [1146696]
+
 docker volume create --name=mnt
 docker-compose up -d --build
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,10 @@
+# COMP90024 Project - Team 34
+# Lokesh Sai Sri Harsha Sankarasetty, Melbourne, [1130612]
+# Kanch Vatcharotayan, Melbourne, [1132855]
+# Sai Deepthi Amancha, Melbourne, [1051388]
+# Josin Saji Abraham, Melbourne, [1129428]
+# Kush Garg, Melbourne, [1146696]
+
 from .database_setup import setup
 from .language_code import LANGUAGE_CODES
 from .parse import Parser
